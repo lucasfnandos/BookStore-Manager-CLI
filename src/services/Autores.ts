f4b07a0f6fb333a52af1b0667008956600c85e6e
lucasfnandos <lucasfsoares@hotmail.com>
2026-07-14 22:06:23 -0300

    feat(autores): updated service functions names

--- src/services/Autores.ts
+++ src/services/Autores.ts
@@ -1,20 +1,20 @@
 import { Autores } from "../models/Autores";
 import { 
-    criarAutor, 
-    buscarAutorPorId,
-    buscarAutorPorNome,
-    atualizarAutor,
-    deletarAutor,
-    existeAutor
+    repositoryCriarAutor, 
+    repositoryBuscarAutorPorId,
+    repositoryBuscarAutorPorNome,
+    repositoryAtualizarAutor,
+    repositoryDeletarAutor,
+    repositoryExisteAutor
 } from "../repositories/Autores";
 
-export async function cadastrarAutor(nome: string, nacionalidade: string, data_nascimento: Date): Promise<Autores> {
-    const autorExiste = await existeAutor(nome, nacionalidade, data_nascimento);
+export async function serviceCriarAutor(nome: string, nacionalidade: string, data_nascimento: Date): Promise<Autores> {
+    const autorExiste = await repositoryExisteAutor(nome, nacionalidade, data_nascimento);
     if (autorExiste) {
         throw new Error("ENTIDADE_JA_EXISTE");
     }
     
-    const id = await criarAutor(nome, nacionalidade, data_nascimento); 
+    const id = await repositoryCriarAutor(nome, nacionalidade, data_nascimento); 
     if (!id) {
         throw new Error("DB_RETORNO_NULO");
     } 
@@ -22,8 +22,8 @@ export async function cadastrarAutor(nome: string, nacionalidade: string, data_n
     return { id, nome, nacionalidade, data_nascimento };
 }
 
-export async function editarAutor(id: number, nome?: string, nacionalidade?: string, data_nascimento?: Date): Promise<Autores> {
-    const autorExistente = await buscarAutorPorId(id);
+export async function serviceAtualizarAutor(id: number, nome?: string, nacionalidade?: string, data_nascimento?: Date): Promise<Autores> {
+    const autorExistente = await repositoryBuscarAutorPorId(id);
     if (!autorExistente) {
         throw new Error("ID_NAO_ENCONTRADO");
     }
@@ -32,7 +32,7 @@ export async function editarAutor(id: number, nome?: string, nacionalidade?: str
     const nacionalidadeFinal = nacionalidade || autorExistente.nacionalidade;
     const dataNascimentoFinal = data_nascimento || autorExistente.data_nascimento;
     
-    const atualizado = await atualizarAutor(id, nomeFinal, nacionalidadeFinal, dataNascimentoFinal);
+    const atualizado = await repositoryAtualizarAutor(id, nomeFinal, nacionalidadeFinal, dataNascimentoFinal);
     if (!atualizado) {
         throw new Error("DB_RETORNO_NULO");
     }
@@ -40,18 +40,18 @@ export async function editarAutor(id: number, nome?: string, nacionalidade?: str
     return { id, nome: nomeFinal, nacionalidade: nacionalidadeFinal, data_nascimento: dataNascimentoFinal };
 }
 
-export async function encontrarAutor(nome: string): Promise<Autores[]> {
-    const autores = await buscarAutorPorNome(nome);
+export async function serviceBuscarAutorPorNome(nome: string): Promise<Autores[]> {
+    const autores = await repositoryBuscarAutorPorNome(nome);
     return autores;
 }
 
-export async function removerAutor(id: number): Promise<boolean> {
-    const autorExistente = await buscarAutorPorId(id);
+export async function serviceDeletarAutor(id: number): Promise<boolean> {
+    const autorExistente = await repositoryBuscarAutorPorId(id);
     if (!autorExistente) {
         throw new Error("ID_NAO_ENCONTRADO");
     }
     
-    const deletado = await deletarAutor(id);
+    const deletado = await repositoryDeletarAutor(id);
     if (!deletado) {
         throw new Error("FALHA_AO_DELETAR");
     }
