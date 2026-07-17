3f091e03a2ab9a61c3542ab5d38ee3ce836fac3e
lucasfnandos <lucasfsoares@hotmail.com>
2026-07-17 10:20:48 -0300

    feat(livros): created Livros.ts and Livros type in models

---
+++ src/models/Livros.ts
@@ -0,0 +1,12 @@
+export interface Livros{
+    id: number;
+    autor_id: number;
+	titulo: string;
+	sub_titulo: string;
+	editora: string;
+	publicado_em: Date;
+	edicao: number;
+	formato: string;
+	isbn: string;
+    genero: string;
+}
