2deaff0d09eaa184f1b842160587b249cbe7d862
lucasfnandos <lucasfsoares@hotmail.com>
2026-07-17 16:39:02 -0300

    feat(livros): created services and implemented its methods

--- src/services/Livros.ts
+++ src/services/Livros.ts
@@ -5,7 +5,8 @@ import {
     repositoryBuscarLivroPorAutorId,
     repositoryAtualizarLivro,
     repositoryDeletarLivro,
-    repositoryExisteLivro
+    repositoryExisteLivro,
+    repositoryBuscarLivroPorTitulo
 } from "../repositories/Livros";
 
 export async function serviceCriarLivro(autor_id:number, titulo:string, editora:string, publicado_em:Date, edicao:number, isbn:string, formato?:string, sub_titulo?:string, genero?:string): Promise<Livros> {
@@ -18,51 +19,61 @@ export async function serviceCriarLivro(autor_id:number, titulo:string, editora:
     const novoGenero = genero || 'Não informado'
     const subTitulo = sub_titulo ?? null
     
-    const id = await repositoryCriarLivro(autor_id, titulo, subTitulo, editora, publicado_em, edicao, novoFormato, isbn, novoGenero); 
+    const id = await repositoryCriarLivro(autor_id, titulo, subTitulo, editora, publicado_em, edicao, novoFormato, isbn, novoGenero) 
     if (!id) {
-        throw new Error("DB_RETORNO_NULO");
-    } 
+        throw new Error("DB_RETORNO_NULO")
+    }
     
-    return { id, autor_id, titulo, sub_titulo, editora, publicado_em, edicao, formato, isbn, genero };
+    return { id, autor_id, titulo, sub_titulo:subTitulo, editora, publicado_em, edicao, formato:novoFormato, isbn, genero:novoGenero }
 }
 
-export async function serviceAtualizarCliente(id: number, nome?: string, cpf?:string, email?:string, contato?:string, data_nascimento?:Date): Promise<Livros> {
-    const clienteExistente = await repositoryBuscarClientePorId(id);
-    if (!clienteExistente) {
+export async function serviceAtualizarLivro(id:number, autor_id?:number, titulo?:string, editora?:string, publicado_em?:Date, edicao?:number, isbn?:string, formato?:string, sub_titulo?:string, genero?:string): Promise<Livros> {
+    const livroExistente = await repositoryBuscarLivroPorId(id);
+    if (!livroExistente) {
         throw new Error("ID_NAO_ENCONTRADO");
     }
     
-    const nomeFinal = nome || clienteExistente.nome;
-    const cpfFinal = cpf || clienteExistente.cpf;
-    const emailFinal = email || clienteExistente.email;
-    const contatoFinal = contato || clienteExistente.contato;
-    const dataNascimentoFinal = data_nascimento || clienteExistente.data_nascimento;
+    const autorIdFinal = autor_id || livroExistente.autor_id
+    const tituloFinal = titulo || livroExistente.titulo
+    const subTituloFinal = sub_titulo || livroExistente.sub_titulo
+    const editoraFinal = editora || livroExistente.editora
+    const publicacaoFinal = publicado_em || livroExistente.publicado_em
+    const edicaoFinal = edicao || livroExistente.edicao
+    const formatoFinal = formato || livroExistente.formato
+    const isbnFinal = isbn || livroExistente.isbn
+    const generoFinal = genero || livroExistente.genero
 
 
     
-    const atualizado = await repositoryAtualizarCliente(id, nomeFinal, cpfFinal, emailFinal, contatoFinal, dataNascimentoFinal);
+    const atualizado = await repositoryAtualizarLivro(id, autorIdFinal, tituloFinal, subTituloFinal, editoraFinal, publicacaoFinal, edicaoFinal, formatoFinal, isbnFinal, generoFinal);
     if (!atualizado) {
         throw new Error("DB_RETORNO_NULO");
     }
     
-    return { id, nome: nomeFinal, cpf: cpfFinal, email: emailFinal, contato: contatoFinal, data_nascimento: dataNascimentoFinal };
+    return { id, autor_id: autorIdFinal, titulo: tituloFinal, sub_titulo: subTituloFinal, editora: editoraFinal, publicado_em: publicacaoFinal, edicao: edicaoFinal, formato: formatoFinal, isbn: isbnFinal, genero: generoFinal };
 }
 
-export async function serviceBuscarClientePorNome(nome: string): Promise<Livros[]> {
-    const clientes = await repositoryBuscarClientePorNome(nome);
-    return clientes;
+export async function serviceBuscarLivroPorTitulo(titulo: string): Promise<Livros[]> {
+    const listaDeLivros = await repositoryBuscarLivroPorTitulo(titulo);
+    return listaDeLivros;
 }
 
-export async function serviceDeletarCliente(id: number): Promise<boolean> {
-    const clienteExistente = await repositoryBuscarClientePorId(id);
-    if (!clienteExistente) {
+export async function serviceDeletarLivro(id: number): Promise<boolean> {
+    const livroExistente = await repositoryBuscarLivroPorId(id);
+    if (!livroExistente) {
         throw new Error("ID_NAO_ENCONTRADO");
     }
-    
-    const deletado = await repositoryDeletarCliente(id);
+    const deletado = await repositoryDeletarLivro(id);
     if (!deletado) {
         throw new Error("FALHA_AO_DELETAR");
     }
-    
     return true;
+}
+
+export async function serviceBuscarLivroPorAutorId(autor_id:number): Promise<Livros[]> {
+    const livrosDoAutor = await repositoryBuscarLivroPorAutorId(autor_id)
+    if(!livrosDoAutor) {
+        throw new Error("ID_NAO_ENCONTRADO")
+    }
+    return livrosDoAutor
 }
