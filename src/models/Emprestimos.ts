aee72c006866827c8aea399c4b32a94fdd7a4623
lucasfnandos <lucasfsoares@hotmail.com>
2026-07-18 21:23:27 -0300

    feat(emprestimos): implemented EsprestimoDetalhado interface

--- src/models/Emprestimos.ts
+++ src/models/Emprestimos.ts
@@ -6,3 +6,11 @@ export interface Emprestimos {
     prazo_devolucao: Date;
     devolvido_em?: Date | null;
 }
+
+export interface EmprestimoDetalhado extends Emprestimos {
+    cliente_nome: string;
+    autor_nome: string;
+    titulo: string;
+    edicao: string;
+    isbn: string;
+}
