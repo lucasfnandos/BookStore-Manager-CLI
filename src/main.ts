cbcc3a1215bd5eb7c48b8cf33d267d8a2c502156
lucasfnandos <lucasfsoares@hotmail.com>
2026-07-19 18:53:13 -0300

    feat(main): minimum changes in main

--- src/main.ts
+++ src/main.ts
@@ -4,19 +4,18 @@ import { iniciarMenuPrincipal } from './menu/menuPrincipal'
 import { pool } from './database/db'
 
 async function main() {
-    const terminal = readline.createInterface({ input, output });
+    const terminal = readline.createInterface({ input, output })
     try {
-        console.log("Iniciando o sistema...");
-        await iniciarMenuPrincipal(terminal);
+        console.log("Iniciando o sistema...")
+        await iniciarMenuPrincipal(terminal)
         
     } catch (error) {
-        console.error("\nOcorreu um erro fatal na aplicação:", error);
+        console.error("\nOcorreu um erro fatal na aplicação:", error)
     } finally {
-        terminal.close();
-        await pool.end();
-        console.log("Conexão com o banco de dados encerrada.");
+        terminal.close()
+        await pool.end()
+        console.log("Conexão com o banco de dados encerrada.")
         process.exit(0);
     }
 }
-
-main();
+main()
